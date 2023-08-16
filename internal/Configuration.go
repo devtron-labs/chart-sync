df29d9b3c953af343858d34391c771693f85f5ea
Ash-exp <asutosh2000ad@gmail.com>
2023-08-16 16:58:38 +0530

    feat: added comments

--- internal/Configuration.go
+++ internal/Configuration.go
@@ -4,7 +4,7 @@ import "github.com/caarlos0/env"
 
 type Configuration struct {
 	AppStoreAppVersionsSaveChunkSize int    `env:"APP_STORE_APPLICATION_VERSIONS_SAVE_CHUNK_SIZE" envDefault:"20"`
-	ChartProviderId                  string `env:"CHART_PROVIDER_ID" envDefault:"*"`
+	ChartProviderId                  string `env:"CHART_PROVIDER_ID" envDefault:"*"` // * is used to sync all chart providers; else CHART_PROVIDER_ID should contain chart_repo_id OR docker_artifact_store_id
 	IsOCIRegistry                    bool   `env:"IS_OCI_REGISTRY" envDefault:"true"`
 }
 
